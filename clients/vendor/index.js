6b83acb8dd92eed50d8acb2d18251c30567019d0
Mak Trnka <maktrnka@comcast.net>
2024-04-17 14:07:44 -0700

    Update index.js

--- clients/vendor/index.js
+++ clients/vendor/index.js
@@ -1,7 +1,13 @@
 // VENDOR ROLE: Responsible for listening to delivery notifications and submitting new packages
 
 const newPackage = require("./handler.js");
-const events = require("../eventPool.js");
+const io = require('socket.io-client');
+
+const url = 'http://localhost:3000/caps';
+const events = io.connect(url);
+
+events.emit('join', { store: '401d59', clientId: 'vendor' });
+events.on('join', console.log);
 
 function makePayload() {
   let payload = newPackage();
@@ -14,5 +20,5 @@ function delivered() {
   });
 }
 
-module.exports = { makePayload, delivered };
-
+delivered();
+makePayload();
